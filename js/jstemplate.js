fff7525a79639e3258d608bff17fc34ee32823d5
Erik Hansen <0216haer@jonkoping.se>
2021-04-19 15:01:27 +0200

    Allt Uppladdat

---
+++ js/jstemplate.js
@@ -0,0 +1,41 @@
+//Darkmode (enbart för större skärmar)
+var foota = document.getElementsByClassName('foota')
+$(document).ready(function() {
+    $(".foota").addClass("lighttext");
+    $('.theme').on("click", function()
+
+        {
+            if ($("body").hasClass("dark")) {
+                $("body").removeClass("dark");
+                $(".header").removeClass("darknav");
+                $(".nava").removeClass("darktext");
+                $("#carti").removeClass("darktext");
+                $(".logo").attr("src", "../pics/logosvartcirk.png");
+                $(".theme").removeClass("darkbutton");
+                $(".theme").removeClass("darkbutton:hover");
+                $("footer").removeClass("darknav");
+                $(".foota").addClass("lighttext");
+                $(".foota").removeClass("darktext");
+
+            } else {
+                $("body").addClass("dark");
+                $(".header").addClass("darknav");
+                $(".nava").addClass("darktext");
+                $("#carti").addClass("darktext");
+                $(".logo").attr("src", "../pics/logovit.png");
+                $(".theme").addClass("darkbutton");
+                $(".theme").addClass("darkbutton:hover");
+                $("footer").addClass("darknav");
+                $(".foota").addClass("darktext");
+                $(".foota").removeClass("lighttext");
+            }
+
+        });
+
+});
+
+//Kontrollera Nav för mobil
+function show() {
+    document.getElementById('navside').classList.toggle('active');
+    burger.classList.toggle('toggle')
+}
