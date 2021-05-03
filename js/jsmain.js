5b25f1ff4fb297b154808f2d8a66b184b82b13f4
Erik Hansen <0216haer@jonkoping.se>
2021-05-03 15:04:44 +0200

    Färdig Huvudsida

--- js/jsmain.js
+++ js/jsmain.js
@@ -1,9 +1,11 @@
-$(document).ready(function(){
-    $("#textsection").on("click", function(){
-        $("#flscreen").css({"display": "block"})
+$(document).ready(function() {
+    $("#textsection").on("click", function() {
+        $("#flscreen").css({ "display": "block" })
     })
-    $("#flscreen").on("click", function(){
-        $("#flscreen").css({"display": "none"})
+    $("#textsectionmob").on("click", function() {
+        $("#flscreen").css({ "display": "block" })
+    })
+    $("#flscreen").on("click", function() {
+        $("#flscreen").css({ "display": "none" })
     })
 })
-
